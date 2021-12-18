e415b3728389fbcff6ae0e49f526677865454e4f
pipet <felipefelizx@gmail.com>
2021-12-18 13:43:20 -0300

    Share
    funcion share , tengo un warning cuidao

--- Lab.pl
+++ Lab.pl
@@ -6,7 +6,10 @@ estaObjeto(X,[_|R]):-estaObjeto(X,R).
 %obtener el ID (post ,comment ,share)
 %getId(Lista respuestas o preguntas, Id).
 getId([[_,_,_,_,_,Id]|_], Id).
-
+%revisar si concuerdan los ID (share and comment)
+%estaId(Id, Facebook respuestas o preguntas).
+estaId(Id,[[_,_,_,_,_,Id]|_]):-!.
+estaId(Id,[_|Resto]):-estaId(Id,Resto).
 
 %------------------------------------------------------------------------
 %REGISTER
@@ -56,7 +59,7 @@ register([],"pipe","pass",Facebook2),login(Facebook2,"pipe","pass",Facebook3),re
 
 post([[_],[_],[_],[_],[],[_]], _, _, _, _):-false.
 post([Restorar,Add,Share, [], [[User,Pass]], Registrados], Fecha, Publicacion, UserList, [Restorar,Add,Share,[[[User,Pass], Fecha, Publicacion, UserList, 0, 1]], [], Registrados]).
-post([Restorar,Add,Share, Publicaciones, [[User,Pass]], Registrados], Fecha, Publicacion, UserList, [Restorar,Add,Share, [[[User,Pass], Fecha, Publicacion, UserList, 0, IdPost]|Publicaciones], [], Registrados]):-getId(Publicaciones, Id),IdPost is Id+1.
+post([Restorar,Add,Share, Create, [[User,Pass]], Registrados], Fecha, Publicacion, UserList, [Restorar,Add,Share, [[[User,Pass], Fecha, Publicacion, UserList, 0, IdPost]|Create], [], Registrados]):-getId(Create, Id),IdPost is Id+1.
 /**Ejemplos de uso**
 
 register([],"pipe","pass",Facebook2),login(Facebook2,"pipe","pass",Facebook3),post(Facebook3, "1.1.2021", "publicacion1", ["user1","user2"], Facebook4).
@@ -71,3 +74,25 @@ register([],"pipe","pass",Facebook2),login(Facebook2,"pipe","pass",Facebook3),re
 */
 /*y como funciona?*/
 %registras a pipe , login pipe , post pipe
+
+%------------------------------------------------------------------------------------
+%Share
+%------------------------------------------------------------------------------------
+%funcionalidad:Stack que permite a un usuario compatir un post de otra persona 
+%Dom: fecha(string) , ID(Number) , UserList(Lista de string)
+%Rec: Post(String)
+
+share([_,_,[],_, _, _], _, _, _, _):-false.
+share([Restorar,Add,Share, Create, [[User,Pass]], Registrados], Fecha, IdShare, UserList, [Restorar,Add,[[[User,Pass], Fecha, IdShare, UserList, 1, 1]], Create, [], Registrados]).
+share([Restorar,Add,Share,Create, [[User,Pass]], Registrados], Fecha, IdShare, UserList, [Restorar,Add,[[[User,Pass], Fecha, IdShare,UserList,0,idPost]|Share],Create, [], Registrados])
+:-estaId(IdShare, Create),getId(Share, Id),IdShare is Id+1.
+
+/**ejemplo de uso**
+
+register([],"Pipe","pass",Facebook2),register(Facebook2,"juan","pass2",Facebook3),login(Facebook3,"Pipe","pass",Facebook4),post(Facebook4, "1.1.2021", "Publicacion1", ["user1","user2"], Facebook5),login(Facebook6,"juan","pass2",Facebook7), share(Facebook7, "2.3.2021" , 1 , ["user2"], Facebook8 ).
+
+*/
+
+/*y como funciona?*/
+%registras a pipe , registras a juan , ingresa pipe , postea pipe , ingresa juan , comparte lo de pipe
+%buscador de id si existe , aplicar funcion Post
