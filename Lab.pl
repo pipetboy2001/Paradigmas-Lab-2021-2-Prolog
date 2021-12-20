dff6c890cc250f9bd622b1efe624313c8b99837d
pipet <felipefelizx@gmail.com>
2021-12-20 16:45:14 -0300

    Arreglado ADD
    ME LA SALVE AA

--- Lab.pl
+++ Lab.pl
@@ -3,11 +3,11 @@
 estaObjeto(X,[X|_]):-!.
 estaObjeto(X,[_|R]):-estaObjeto(X,R).
 
-%obtener el ID (post ,comment ,share)
+%obtener el ID (post ,Add ,share)
 %getId(Lista respuestas o preguntas, Id).
 getId([[_,_,_,_,_,Id]|_], Id).
-%revisar si concuerdan los ID (share and comment)
-%estaId(Id, Facebook respuestas o preguntas).
+%revisar si concuerdan los ID (share and Add)
+%estaId(Id, GoogleDoc respuestas o preguntas).
 estaId(Id,[[_,_,_,_,_,Id]|_]):-!.
 estaId(Id,[_|Resto]):-estaId(Id,Resto).
 
@@ -25,10 +25,10 @@ register([[_],[_],[_],[_],[_],[[User,Pass]]], User, Pass, [[_],[_],[_],[_],[_],[
 register([Restorar,Add,Share,Create,UsuariosActivos,Registrados], User, Pass, [Restorar,Add,Share,Create,UsuariosActivos,[[User,Pass,10]|Registrados]]).
 /**Ejemplo de uso: **
 
-register([],"pipe","pass",Facebook2).
-register([],"pipe","pass",Facebook2),register(Facebook2,"user","pass2",Facebook3).
-register([],"pipe","pass",Facebook2),register(Facebook2,"user","pass2",Facebook3),register(Facebook3,"user","pass2",Facebook4).
-register([],"pipe","pass",Facebook2),register(Facebook2,"user","pass2",Facebook3),register(Facebook3,"user","pass2",Facebook4),register(Facebook4,"user","pass2",Facebook5).
+register([],"pipe","pass",GoogleDoc2).
+register([],"pipe","pass",GoogleDoc2),register(GoogleDoc2,"user","pass2",GoogleDoc3).
+register([],"pipe","pass",GoogleDoc2),register(GoogleDoc2,"user","pass2",GoogleDoc3),register(GoogleDoc3,"user","pass2",GoogleDoc4).
+register([],"pipe","pass",GoogleDoc2),register(GoogleDoc2,"user","pass2",GoogleDoc3),register(GoogleDoc3,"user","pass2",GoogleDoc4),register(GoogleDoc4,"user","pass2",GoogleDoc5).
 
 */
 
@@ -43,10 +43,10 @@ login([_,_,_,_,_,[]], _, _, _):-false.
 login([Restorar,Add,Share,Create, UsuariosActivos, Registrados], User, Pass, [Restorar,Add,Share,Create, [[User,Pass]|UsuariosActivos], Registrados]):-estaObjeto([User,Pass,_], Registrados).
 /**Ejemplos de uso**
 
-register([],"pipe","pass",Facebook2),login(Facebook2,"pipe","pass",Facebook3).
-register([],"pipe","pass",Facebook2),register(Facebook2,"user","pass2",Facebook3),login(Facebook3,"pipe","pass",Facebook4).
-register([],"pipe","pass",Facebook2),login(Facebook2,"pipe","pass",Facebook3),register(Facebook3,"user","pass2",Facebook4).
-register([],"pipe","pass",Facebook2),login(Facebook2,"pipe","pass",Facebook3),register(Facebook3,"user","pass2",Facebook4),login(Facebook4,"pipe","pass",Facebook5).
+register([],"pipe","pass",GoogleDoc2),login(GoogleDoc2,"pipe","pass",GoogleDoc3).
+register([],"pipe","pass",GoogleDoc2),register(GoogleDoc2,"user","pass2",GoogleDoc3),login(GoogleDoc3,"pipe","pass",GoogleDoc4).
+register([],"pipe","pass",GoogleDoc2),login(GoogleDoc2,"pipe","pass",GoogleDoc3),register(GoogleDoc3,"user","pass2",GoogleDoc4).
+register([],"pipe","pass",GoogleDoc2),login(GoogleDoc2,"pipe","pass",GoogleDoc3),register(GoogleDoc3,"user","pass2",GoogleDoc4),login(GoogleDoc4,"pipe","pass",GoogleDoc5).
 */
 
 %-----------------------------------------------------------------------------------
@@ -62,13 +62,13 @@ post([Restorar,Add,Share, [], [[User,Pass]], Registrados], Fecha, Publicacion, U
 post([Restorar,Add,Share, Create, [[User,Pass]], Registrados], Fecha, Publicacion, UserList, [Restorar,Add,Share, [[[User,Pass], Fecha, Publicacion, UserList, 0, IdPost]|Create], [], Registrados]):-getId(Create, Id),IdPost is Id+1.
 /**Ejemplos de uso**
 
-register([],"pipe","pass",Facebook2),login(Facebook2,"pipe","pass",Facebook3),post(Facebook3, "1.1.2021", "publicacion1", ["user1","user2"], Facebook4).
+register([],"pipe","pass",GoogleDoc2),login(GoogleDoc2,"pipe","pass",GoogleDoc3),post(GoogleDoc3, "1.1.2021", "publicacion1", ["user1","user2"], GoogleDoc4).
 
-register([],"user1","pass",Facebook2),login(Facebook2,"user1","pass",Facebook3),post(Facebook3, "1.3.2021", "Existe?", ["user1","user2"], Facebook4).
+register([],"user1","pass",GoogleDoc2),login(GoogleDoc2,"user1","pass",GoogleDoc3),post(GoogleDoc3, "1.3.2021", "Existe?", ["user1","user2"], GoogleDoc4).
 
-register([],"Juan","pass",Facebook2),login(Facebook2,"Juan","pass",Facebook3),post(Facebook3, "1.3.2021", "que lindo dia", ["user1","user2"], Facebook4).
+register([],"Juan","pass",GoogleDoc2),login(GoogleDoc2,"Juan","pass",GoogleDoc3),post(GoogleDoc3, "1.3.2021", "que lindo dia", ["user1","user2"], GoogleDoc4).
 
-register([],"pipe","pass",Facebook2),login(Facebook2,"pipe","pass",Facebook3),register(Facebook3,"user","pass2",Facebook4),login(Facebook4,"pipe","pass",Facebook5),post(Facebook6, "1.3.2021", "esto es un post", ["user1","user2"], Facebook7).
+register([],"pipe","pass",GoogleDoc2),login(GoogleDoc2,"pipe","pass",GoogleDoc3),register(GoogleDoc3,"user","pass2",GoogleDoc4),login(GoogleDoc4,"pipe","pass",GoogleDoc5),post(GoogleDoc6, "1.3.2021", "esto es un post", ["user1","user2"], GoogleDoc7).
 **
 
 */
@@ -83,16 +83,33 @@ register([],"pipe","pass",Facebook2),login(Facebook2,"pipe","pass",Facebook3),re
 %Rec: Post(String)
 
 share([_,_,[],_, _, _], _, _, _, _):-false.
-share([Restorar,Add,Share, Create, [[User,Pass]], Registrados], Fecha, IdShare, UserList, [Restorar,Add,[[[User,Pass], Fecha, IdShare, UserList, 1, 1]], Create, [], Registrados]).
+share([Restorar,Add,Share, Create, [[User,Pass]], Registrados], Fecha, IdShare, UserList, [Restorar,Add,[[[User,Pass], Fecha, IdShare, UserList, 1, 1]|Share], Create, [], Registrados]).
 share([Restorar,Add,Share,Create, [[User,Pass]], Registrados], Fecha, IdShare, UserList, [Restorar,Add,[[[User,Pass], Fecha, IdShare,UserList,0,idPost]|Share],Create, [], Registrados])
 :-estaId(IdShare, Create),getId(Share, Id),IdShare is Id+1.
 
 /**ejemplo de uso**
 
-register([],"Pipe","pass",Facebook2),register(Facebook2,"juan","pass2",Facebook3),login(Facebook3,"Pipe","pass",Facebook4),post(Facebook4, "1.1.2021", "Publicacion1", ["user1","user2"], Facebook5),login(Facebook6,"juan","pass2",Facebook7), share(Facebook7, "2.3.2021" , 1 , ["user2"], Facebook8 ).
+register([],"Pipe","pass",GoogleDoc2),register(GoogleDoc2,"juan","pass2",GoogleDoc3),login(GoogleDoc3,"Pipe","pass",GoogleDoc4),post(GoogleDoc4, "1.1.2021", "Publicacion1", ["user1","user2"], GoogleDoc5),login(GoogleDoc6,"juan","pass2",GoogleDoc7), share(GoogleDoc7, "2.3.2021" , 1 , ["user2"], GoogleDoc8 ).
 
 */
 
 /*y como funciona?*/
 %registras a pipe , registras a juan , ingresa pipe , postea pipe , ingresa juan , comparte lo de pipe
 %buscador de id si existe , aplicar funcion Post
+
+%----------------------------------
+%add 
+%----------------------------------
+%funcionalidad:Stack que permite a un usuario comentar un post de otra persona  
+%Dom: idPost x UserList (list de string) x fecha
+%Rec: date X postID X string (comentario)
+
+add([_,[],_,_, _, _], _, _, _, _, _):-false.
+add([Restorar,Add,Share, Create, [[User,Pass]], Registrados], Fecha, IdPost, Comenta, UserList, [Restorar,[[[User,Pass], Fecha, Comenta, UserList, IdPost, 1]|Add],Share,Create, [], Registrados]).
+add([Restorar,Add,Share,Create, [[User,Pass]], Registrados], Fecha, IdPost, Comenta, UserList, [Restorar,[[[User,Pass], Fecha, Comenta, UserList, IdPost, IdRespuesta]|Add],Share,Create, [], Registrados])
+:-estaId(IdPost, Create),getId(Add, Id), IdRespuesta is Id+1.
+/**Ejemplos de uso**
+
+register([],"pipe","pass",GoogleDoc2),register(GoogleDoc2,"user","pass2",GoogleDoc3),login(GoogleDoc3,"pipe","pass",GoogleDoc4),post(GoogleDoc4, "1.1.2021", "pregunta1", ["user1","user2"], GoogleDoc5),login(GoogleDoc5,"user","pass2",GoogleDoc6),add(GoogleDoc6, "2.1.2020", 1, "en efecto", ["user1", "user2", "user3"], GoogleDoc7).
+
+*/
