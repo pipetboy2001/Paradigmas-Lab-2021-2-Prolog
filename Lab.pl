83d42acbe8a06e8d2f2f257c47aadc7c07f5bc41
pipet <felipefelizx@gmail.com>
2021-12-27 00:01:53 -0300

    Restorar (NOT WORKING)
    :( siempre uno me falla bien feo

--- Lab.pl
+++ Lab.pl
@@ -11,6 +11,21 @@ getId([[_,_,_,_,_,Id]|_], Id).
 estaId(Id,[[_,_,_,_,_,Id]|_]):-!.
 estaId(Id,[_|Resto]):-estaId(Id,Resto).
 
+cambiarEstado(Id, User, Pass, ListaOriginal, [[[User, Pass],Fecha,Pregunta,Tags, _, Id]|_], [[[User, Pass],Fecha,Pregunta,Tags, 1, Id]|PreguntasEdit]):-borrarObjeto([[User, Pass],Fecha,Pregunta,Tags, _, Id], ListaOriginal, PreguntasEdit),!.
+cambiarEstado(Id, User, Pass, ListaOriginal, [_|Create], Resultado):-cambiarEstado(Id, User, Pass, ListaOriginal,Create, Resultado).
+
+getAutor(Id, [[[User, Pass], _,_,_,_,Id]|_], User, Pass):-!.
+getAutor(Id, [_|Resto], User, Pass):-getAutor(Id, Resto, User, Pass).
+
+
+borrarObjeto(X, [X|T], T).
+borrarObjeto(X, [H|T], [H|TailEdit]):-borrarObjeto(X, T, TailEdit).
+
+addRep(User, Pass, Cantidad, ListaOriginal, [[User, Pass, Rep]|_], [[User,Pass,RepEdit]|UsuariosEditado]):-borrarObjeto([User, Pass, Rep], ListaOriginal, UsuariosEditado), RepEdit is Cantidad + Rep, !.
+addRep(User, Pass, Cantidad, ListaOriginal, [_|Registrados], Resultado):-addRep(User,Pass,Cantidad,ListaOriginal,Registrados, Resultado).
+
+
+
 %------------------------------------------------------------------------
 %REGISTER
 %------------------------------------------------------------------------
@@ -35,7 +50,7 @@ register([],"pipe","pass",GoogleDoc2),register(GoogleDoc2,"user","pass2",GoogleD
 %-----------------------------------------------------------------------------------
 %login (socialNetworkLogin)
 %---------------------------------------------------------------------------------
-%funcionalidad:Stack que permite a un usuario ingresar en la red social
+%funcionalidad:GoogleDoc que permite a un usuario ingresar en la red social
 %Dom: User(String) , Pass(String)
 %Rec: User(String) , Pass (String)
 
@@ -53,7 +68,7 @@ register([],"pipe","pass",GoogleDoc2),login(GoogleDoc2,"pipe","pass",GoogleDoc3)
 %post (socialNetworkLogin)
 %---------------------------------------------------------------------------------
 
-%Funcionalidad:stack que permite publicar una publicacion
+%Funcionalidad:GoogleDoc que permite publicar una publicacion
 %DOM : Fecha (String ), Publicacion(String), UserList(Lista de String)
 %REC : Fecha (String) , Publicacion (String)
 
@@ -78,18 +93,18 @@ register([],"pipe","pass",GoogleDoc2),login(GoogleDoc2,"pipe","pass",GoogleDoc3)
 %------------------------------------------------------------------------------------
 %Share
 %------------------------------------------------------------------------------------
-%funcionalidad:Stack que permite a un usuario compatir un post de otra persona 
+%funcionalidad:GoogleDoc que permite a un usuario compatir un post de otra persona 
 %Dom: fecha(string) , ID(Number) , UserList(Lista de string)
 %Rec: Post(String)
 
 share([_,_,[],_, _, _], _, _, _, _):-false.
-share([Restorar,Add,Share, Create, [[User,Pass]], Registrados], Fecha, IdShare, UserList, [Restorar,Add,[[[User,Pass], Fecha, IdShare, UserList, 1, 1]|Share], Create, [], Registrados]).
-share([Restorar,Add,Share,Create, [[User,Pass]], Registrados], Fecha, IdShare, UserList, [Restorar,Add,[[[User,Pass], Fecha, IdShare,UserList,0,idPost]|Share],Create, [], Registrados])
+share([Restorar,Add,Share, Create, [[User,Pass]], Registrados], Fecha, IdShare, Permiso, [Restorar,Add,[[[User,Pass], Fecha, IdShare, Permiso, 1, 1]|Share], Create, [], Registrados]).
+share([Restorar,Add,Share,Create, [[User,Pass]], Registrados], Fecha, IdShare, Permiso, [Restorar,Add,[[[User,Pass], Fecha, IdShare,Permiso,0,idPost]|Share],Create, [], Registrados])
 :-estaId(IdShare, Create),getId(Share, Id),IdShare is Id+1.
 
 /**ejemplo de uso**
 
-register([],"Pipe","pass",GoogleDoc2),register(GoogleDoc2,"juan","pass2",GoogleDoc3),login(GoogleDoc3,"Pipe","pass",GoogleDoc4),post(GoogleDoc4, "1.1.2021", "Publicacion1", ["user1","user2"], GoogleDoc5),login(GoogleDoc6,"juan","pass2",GoogleDoc7), share(GoogleDoc7, "2.3.2021" , 1 , ["user2"], GoogleDoc8 ).
+register([],"Pipe","pass",GoogleDoc2),register(GoogleDoc2,"juan","pass2",GoogleDoc3),login(GoogleDoc3,"Pipe","pass",GoogleDoc4),post(GoogleDoc4, "1.1.2021", "Publicacion1", ["user1","user2"], GoogleDoc5),login(GoogleDoc6,"juan","pass2",GoogleDoc7), share(GoogleDoc7, "2.3.2021" , 1 , "R", GoogleDoc8 ).
 
 */
 
@@ -100,7 +115,7 @@ register([],"Pipe","pass",GoogleDoc2),register(GoogleDoc2,"juan","pass2",GoogleD
 %----------------------------------
 %add 
 %----------------------------------
-%funcionalidad:Stack que permite a un usuario comentar un post de otra persona  
+%funcionalidad:GoogleDoc que permite a un usuario comentar un post de otra persona  
 %Dom: idPost x UserList (list de string) x fecha
 %Rec: date X postID X string (comentario)
 
@@ -114,3 +129,36 @@ register([],"pipe","pass",GoogleDoc2),register(GoogleDoc2,"user","pass2",GoogleD
 
 */
 
+%----------------------------------
+%Restorar
+%----------------------------------
+%funcionalidad
+%Dom ParadigmaDocs X int X int X ParadigmaDocs 
+%Rec
+
+restorar([[],_,_,[], _, _], _, _, _, _):-false.
+
+
+restorar([Restorar,Add,Share, Create, [[User,Pass]], Registrados], IdPost, NewId, [Add, CreateEditado, [], RegistradosEditado2] ):-
+estaId(IdPost, Create), estaId(NewId, Add),
+  cambiarEstado(IdPost, User, Pass, Create, Create, CreateEditado), getAutor(NewId, Add, UserAutorRespuesta, PassAutorRespuesta),
+  addRep(UserAutorRespuesta, PassAutorRespuesta, 15, Registrados, Registrados, RegistradosEditado), addRep(User,Pass,2,RegistradosEditado, RegistradosEditado, RegistradosEditado2).
+
+
+
+
+%register([],"pipe","pass",GoogleDoc2),register(GoogleDoc2,"user","pass2",GoogleDoc3),login(GoogleDoc3,"pipe","pass",GoogleDoc4),post(GoogleDoc4, "1.1.2021", "pregunta1", ["user1","user2"], GoogleDoc5),login(GoogleDoc5,"user","pass2",GoogleDoc6),add(GoogleDoc6, "2.1.2020", 1, "en efecto", ["user1", "user2", "user3"], GoogleDoc7),restore(GoogleDoc7,1,2,GoogleDoc8).
+
+%primero el idPost a
+%luego que devuelvemos
+%parametros 0 a 1
+
+%restorar([[[["user1","pass1"], "1.1.2020", "Pregunta1", ["tag1"]]], [[["user2", "pass2"], "2.1.2020", "ola?", ["tag1"], 0, 1]], [["user2", "pass2"]], [["user1", "pass1", 10], ["user2", "pass2", 10]]], 1, 1, GoogleDoc2).
+
+
+
+%-------------------------------------
+% paradigmaDocsSearch:
+%...................................
+%dom paradigmaDocs X string X List Document 
+%rec 
