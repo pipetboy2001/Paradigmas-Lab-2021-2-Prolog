65137732045ddffd390d788b7eaed88f32599386
pipet <felipefelizx@gmail.com>
2021-12-28 18:37:57 -0300

    documentacion
    de paso arreglo cosas que quedaron del lab del semestre pasado xd

--- Lab.pl
+++ Lab.pl
@@ -1,28 +1,71 @@
+%LAB 2 FELIPE FERNANDEZ
+
+%TDAs
+/*TDA Doc general:
+- Representación: doc que contiene los listados de creacion de doc y los usuarios en sesión activa y usuarios registrados.
+  TDA Usuario:
+- Representación: lista que contiene los strings usuario, contraseña.
+  TDA Create:
+- Representación: doc que contiene autor, string fecha, string de lo que se escriba, lista de usuarios que lo ven, estado (Abierto 0 o cerrado 1), id pregunta.
+- Modificadores: cambiarEstado(Id del doc, usuario autor, contraseña,nuevo doc, doc de preguntas editada).
+- Selectores: getId(stackPregunta, IdObtenido). getAutor(Id de la pregunta, stackPreguntas, Nombre de usuario del autor, contraseña).
+  TDA Respuesta:
+- Representación: doc que contiene autor, string fecha, string respuesta, lista de usuarios, id de la pregunta respondida, id de la respuesta.
+- Selectores: getId(stackRespuesta, IdObtenido). getAutor(Id de la respuesta, stackRespuestas, Nombre de usuario del autor, contraseña).
+*/
+
+%Predicados secundarios
+/*
+estaObjeto(objeto a borrar, lista).
+getId(Lista Doc creado, Id).
+getAutor(Id, stack Doc creado, nombre de usuario, contraseña del usuario).
+borrarObjeto(objeto a borrar, lista, lista sin objeto).
+cambiarEstado(Id de la pregunta, Usuario autor, contraseña, CreateCambiado, CreateCambiado, Create Cambiado con la pregunta cerrada).
+cambioDocActual(Usuario, contraseña, nuevo cambio, lista de usuario, lista de usuario, lista de usuario con el cambio del usuario).
+estaId(Id, stack Doc creado).
+*/
+/*
+register(Docinicial, nombre de usuario, contraseña, DocFinal).
+login(Docinicial, nombre de usuario, contraseña, DocFinal).
+Create(Docinicial, fecha, pregunta, lista de tags, DocFinal).
+Share(DocInicial,fecha,id del doc, permiso,DocFinal)
+Add(Docinicial, fecha, id del doc, agregar palabra, lista de usuario, DocFinal).
+Restore(Docinicial, id de doc actual, id del doc nuevo, DocFinal).
+*/
+%Cláusulas secundarias
+
 %saber si esta lo pedido (login)
 %estaObjeto(objeto a borrar, lista).
 estaObjeto(X,[X|_]):-!.
 estaObjeto(X,[_|R]):-estaObjeto(X,R).
 
 %obtener el ID (post ,Add ,share)
-%getId(Lista respuestas o preguntas, Id).
+%getId(Lista Doc creado, Id).
 getId([[_,_,_,_,_,Id]|_], Id).
 %revisar si concuerdan los ID (share and Add)
-%estaId(Id, GoogleDoc respuestas o preguntas).
+%estaId(Id, GoogleDoc Doc creado).
 estaId(Id,[[_,_,_,_,_,Id]|_]):-!.
 estaId(Id,[_|Resto]):-estaId(Id,Resto).
 
+%cambiar el estado 
+%cambiarEstado(Id de la pregunta, Usuario autor, contraseña, CreateCambiado, CreateCambiado, Create Cambiado con la pregunta cerrada).
 cambiarEstado(Id, User, Pass, ListaOriginal, [[[User, Pass],Fecha,Pregunta,Tags, _, Id]|_], [[[User, Pass],Fecha,Pregunta,Tags, 1, Id]|PreguntasEdit]):-borrarObjeto([[User, Pass],Fecha,Pregunta,Tags, _, Id], ListaOriginal, PreguntasEdit),!.
 cambiarEstado(Id, User, Pass, ListaOriginal, [_|Create], Resultado):-cambiarEstado(Id, User, Pass, ListaOriginal,Create, Resultado).
 
+%obtener el autor o creador (como se le quiera decir)
+%GetAutor(Id, stack Doc creado, nombre de usuario, contraseña del usuario)
 getAutor(Id, [[[User, Pass], _,_,_,_,Id]|_], User, Pass):-!.
 getAutor(Id, [_|Resto], User, Pass):-getAutor(Id, Resto, User, Pass).
 
-
+%BORRA ANTERIOR
+%borrarObjeto(objeto a borrar, lista, lista sin objeto).
 borrarObjeto(X, [X|T], T).
 borrarObjeto(X, [H|T], [H|TailEdit]):-borrarObjeto(X, T, TailEdit).
 
-addRep(User, Pass, Cantidad, ListaOriginal, [[User, Pass, Rep]|_], [[User,Pass,RepEdit]|UsuariosEditado]):-borrarObjeto([User, Pass, Rep], ListaOriginal, UsuariosEditado), RepEdit is Cantidad + Rep, !.
-addRep(User, Pass, Cantidad, ListaOriginal, [_|Registrados], Resultado):-addRep(User,Pass,Cantidad,ListaOriginal,Registrados, Resultado).
+%cambia el estado al anterior segun el ID que se le indique
+%cambioDocActual(Usuario, contraseña, nuevo cambio, lista de usuario, lista de usuario, lista de usuario con el cambio del usuario).
+cambioDocActual(User, Pass, Cantidad, ListaOriginal, [[User, Pass, Rep]|_], [[User,Pass,RepEdit]|UsuariosEditado]):-borrarObjeto([User, Pass, Rep], ListaOriginal, UsuariosEditado), RepEdit is Cantidad + Rep, !.
+cambioDocActual(User, Pass, Cantidad, ListaOriginal, [_|Registrados], Resultado):-cambioDocActual(User,Pass,Cantidad,ListaOriginal,Registrados, Resultado).
 
 
 
@@ -134,31 +177,23 @@ register([],"pipe","pass",GoogleDoc2),register(GoogleDoc2,"user","pass2",GoogleD
 %----------------------------------
 %funcionalidad
 %Dom ParadigmaDocs X int X int X ParadigmaDocs 
-%Rec
-
 restorar([[],_,_,[], _, _], _, _, _, _):-false.
-
-
-restorar([Restorar,Add,Share, Create, [[User,Pass]], Registrados], IdPost, NewId, [Add, CreateEditado, [], RegistradosEditado2] ):-
+restorar([_,Add,_, Create, [[User,Pass]], Registrados], IdPost, NewId, [Add, CreateEditado, [], RegistradosEditado2] ):-
 estaId(IdPost, Create), estaId(NewId, Add),
   cambiarEstado(IdPost, User, Pass, Create, Create, CreateEditado), getAutor(NewId, Add, UserAutorRespuesta, PassAutorRespuesta),
-  addRep(UserAutorRespuesta, PassAutorRespuesta, 15, Registrados, Registrados, RegistradosEditado), addRep(User,Pass,2,RegistradosEditado, RegistradosEditado, RegistradosEditado2).
-
-
-
-
-%register([],"pipe","pass",GoogleDoc2),register(GoogleDoc2,"user","pass2",GoogleDoc3),login(GoogleDoc3,"pipe","pass",GoogleDoc4),post(GoogleDoc4, "1.1.2021", "pregunta1", ["user1","user2"], GoogleDoc5),login(GoogleDoc5,"user","pass2",GoogleDoc6),add(GoogleDoc6, "2.1.2020", 1, "en efecto", ["user1", "user2", "user3"], GoogleDoc7),restore(GoogleDoc7,1,2,GoogleDoc8).
+  cambioDocActual(UserAutorRespuesta, PassAutorRespuesta, 15, Registrados, Registrados, RegistradosEditado), cambioDocActual(User,Pass,2,RegistradosEditado, RegistradosEditado, RegistradosEditado2).
 
 %primero el idPost a
 %luego que devuelvemos
 %parametros 0 a 1
+/*ejemplo de uso 
 
-%restorar([[[["user1","pass1"], "1.1.2020", "Pregunta1", ["tag1"]]], [[["user2", "pass2"], "2.1.2020", "ola?", ["tag1"], 0, 1]], [["user2", "pass2"]], [["user1", "pass1", 10], ["user2", "pass2", 10]]], 1, 1, GoogleDoc2).
+restorar([[[["user1","pass1"], "1.1.2020", "Pregunta1", ["tag1"]]], [[["user2", "pass2"], "2.1.2020", "ola?", ["tag1"], 0, 1]], [["user2", "pass2"]], [["user1", "pass1"], ["user2", "pass2"]]], 1, 1, GoogleDoc2).
 
+*/
 
 
 %-------------------------------------
 % paradigmaDocsSearch:
 %...................................
 %dom paradigmaDocs X string X List Document 
-%rec 
